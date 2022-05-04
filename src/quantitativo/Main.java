728c9fe13d2c18a8b96881325143afd9e7f6fcef
buamaral <bamaralqa@gmail.com>
2022-05-04 17:50:29 -0300

    nLatasTinta arredondado em caso do número não inteiro

--- src/quantitativo/Main.java
+++ src/quantitativo/Main.java
@@ -1,35 +1,34 @@
+package quantitativo;
+
+import java.util.Scanner;
+
 public class Main {
-
-
     public static void main(String[] args) {
 
-        double base;
-        double altura;
         double area;
-        double precoUnitarioTinta;
-
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("=== Calculadora de tinta ===");
-        System.out.println("\\nPara uma área retangular, \\nInforme o tamanho da base, em metros: ");
-        String base = scanner.next();
+        System.out.println("Para uma área retangular, \nInforme o tamanho da base, em metros: ");
+        double base = Double.parseDouble(scanner.next());
         System.out.println("Informe o tamanho da altura, em metros: ");
-        String altura = scanner.next();
-        System.out.println("Informe o valor unitário da tinta de 5L: ")
-        String precoUnitarioTinta = scanner.next();
+        double altura = Double.parseDouble(scanner.next());
+        System.out.println("Informe o valor unitário da tinta de 5L: ");
+        double precoUnitarioTinta = Double.parseDouble(scanner.next());
 
         double custoTotalTinta;
-        double nLatasTinta;
 
         area = base * altura;
 
-        nLatasTinta = area / 15;
+        double qntLatasTinta = area / 15;
+        double nLatasTinta = Math.ceil(qntLatasTinta);
 
         custoTotalTinta = nLatasTinta * precoUnitarioTinta;
 
-        System.out.println("\\nÁrea do local: " + area + "m²");
-        System.out.println("\\nQuantidade de galões de 5l necessários para pintar essa área: " + nLatasTinta + " galões")
-        System.out.println("\\nValor à ser gasto com os galões de tinta: R$ " + custoTotalTinta);
+        System.out.println("Área do local: " + area + "m²");
+        System.out.println("Quantidade de galões de 5l necessários para pintar essa área: "
+                + nLatasTinta + " galões");
+        System.out.println("Valor à ser gasto com os galões de tinta: R$ " + custoTotalTinta);
 
     }
 }
